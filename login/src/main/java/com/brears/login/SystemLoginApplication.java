5e44b0f69198d96650aacebc83a167ecb316daa8
SamAparicioDev <samapariciodev@gmail.com>
2025-04-28 15:49:42 -0500

    first commit

---
+++ login/src/main/java/com/brears/login/SystemLoginApplication.java
@@ -0,0 +1,23 @@
+package com.brears.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SystemLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemLoginApplication.class, args);
+
+	}
+
+
+	@GetMapping
+	public String hello() {
+		return "Hello World";
+	}
+
+}
